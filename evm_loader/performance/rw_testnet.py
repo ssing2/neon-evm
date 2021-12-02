082f37f753decc4efed7c1148eaf7da10bb5b4fc
sinev-valentine <sinev-valentine@yandex.ru>
2021-12-02 13:42:38 +0300

    update rw_testnet.py

--- evm_loader/performance/rw_testnet.py
+++ evm_loader/performance/rw_testnet.py
@@ -20,7 +20,8 @@ ETH_TOKEN_MINT_ID: PublicKey = PublicKey(os.environ.get("ETH_TOKEN_MINT"))
 contract = "6Uu6eoTeAEZf7vfaZW2qN1ZHvvtT4zsn3mp5cukjnnoK"
 contract_code = "8DLZZPX2heYJQnTLkqFCE8CjMZm7t8xkFn2SYp78A34K"
 contract_token = "6avLgmDaPoQaSSwYhsF6jypZzVFL4v8VH45SfDXtNkDD"
-
+contract_eth = "94067940c13f9aac69be38c66dd1e0265f98193e"
+chain_id_testnet = 245022940
 
 class init_wallet():
     def __init__(cls):
@@ -47,15 +48,28 @@ class init_wallet():
         print("Contract:", contract)
         print("Contract code:", contract_code)
         print("Contract token:", contract_token)
+        print("Contract eth:", contract_eth)
 
 
 def get_call_parameters(input, acc, caller, caller_ether):
     nonce = getTransactionCount(client, caller)
-    tx = {'to': contract, 'value': 0, 'gas': 99999999, 'gasPrice': 1_000_000_000,
-          'nonce': nonce, 'data': input, 'chainId': 111}
+    tx = {'to': bytes.fromhex(contract_eth), 'value': 0, 'gas': 99999999, 'gasPrice': 1_000_000_000,
+          'nonce': nonce, 'data': input, 'chainId': chain_id_testnet}
     (from_addr, sign, msg) = make_instruction_data_from_tx(tx, acc.secret_key())
     assert (from_addr == caller_ether)
     return (from_addr, sign, msg, nonce)
+
+
+def create_storage_account( seed, acc):
+    storage = PublicKey(sha256(bytes(acc.public_key()) + bytes(seed, 'utf8') + bytes(PublicKey(evm_loader_id))).digest())
+    print("Storage", storage)
+
+    if getBalance(storage) == 0:
+        trx = Transaction()
+        trx.add(createAccountWithSeed(acc.public_key(), acc.public_key(), seed, 10**9, 128*1024, PublicKey(evm_loader_id)))
+        send_transaction(client, trx, acc)
+
+    return storage
 
 
 wallet = init_wallet()
@@ -63,10 +77,9 @@ wallet = init_wallet()
 func_name = abi.function_signature_to_4byte_selector('unchange_storage(uint8,uint8)')
 input = (func_name + bytes.fromhex("%064x" % 0x1) + bytes.fromhex("%064x" % 0x1))
 
-# (from_addr1, sign1, msg1, _) = self.get_call_parameters(input, self.acc1, self.caller1, self.caller1_ether)
+(from_addr, sign, msg, _) = get_call_parameters(input, wallet.acc, wallet.caller, wallet.caller_ether)
 
-# instruction1 = from_addr1 + sign1 + msg1
-#
-# storage1 = self.create_storage_account(sign1[:8].hex(), self.acc1)
+instruction = from_addr + sign + msg
 
+storage = create_storage_account(sign[:8].hex(), wallet.acc)
 
